885ccd6dc27296b95468049a330393f675a01657
WillB01 <william.hello@outlook.com>
2018-11-13 00:33:17 +0100

    working on mobil view

--- src/components/UI/PlayerInfo/PlayerInfo.js
+++ src/components/UI/PlayerInfo/PlayerInfo.js
@@ -4,26 +4,36 @@ import styles from './PlayerInfo.module.css';
 
 class PlayerInfo extends Component {
     state = {
-        
+        showContent: false
     }
+
+    handleClick = () => this.setState(prevState => ({showContent: !prevState.showContent}));
+
+ 
     render() {
+        const content = (
+            <React.Fragment>
+            {this.props.hasRank ? 
+            <div className={styles.Box}>  
+                <h3>{this.props.triviaMain.rank}</h3> 
+            </div> : null}
+          
+            <div className={styles.Box}>
+                <h3> Total: {this.props.triviaMain.score.total} </h3>
+            </div>
+            <div className={styles.Box}>
+                <h3>Points: {this.props.triviaMain.score.completedQuestionsBonus}</h3>   
+            </div>
+            </React.Fragment>
+            )
         return(
             <div className={styles.PlayerInfoContainer}>
                 <div className={`${styles.Stats} bounce `}>
-                    <div className={styles.Box}>
+                    <div className={styles.Box}
+                         onClick={this.handleClick}>
                         <h3>{this.props.triviaMain.name}</h3>
                     </div>
-                    {this.props.hasRank ? 
-                    <div className={styles.Box}>  
-                        <h3>{this.props.triviaMain.rank}</h3> 
-                    </div> : null}
-                  
-                    <div className={styles.Box}>
-                        <h3> Completed: {this.props.triviaMain.score.total} </h3>
-                    </div>
-                    <div className={styles.Box}>
-                        <h3>Points: {this.props.triviaMain.score.completedQuestionsBonus}</h3>   
-                    </div>
+                    {this.state.showContent ? content : null}                   
                 </div>
             </div>
         );
